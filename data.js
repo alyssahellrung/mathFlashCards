6080585ebc8688be71b69cbef19e81649206e3d2
alyssahellrung <alyssahellrung@gmail.com>
2021-02-17 22:52:30 -0800

    added another script link in html, started data.js

---
+++ data.js
@@ -0,0 +1,345 @@
+const questions = [
+  {
+    "question": "0 X 1", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 2", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 3", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 4", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 5", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 5", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 6", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 7", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 8", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 9", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 10", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 11", 
+    "answer": "0"
+  },
+  {
+    "question": "0 X 12", 
+    "answer": "0"
+  },
+  {
+    "question": "1 X 1", 
+    "answer": "1"
+  },
+  {
+    "question": "1 X 2", 
+    "answer": "2"
+  },
+  {
+    "question": "1 X 3", 
+    "answer": "3"
+  },
+  {
+    "question": "1 X 4", 
+    "answer": "4"
+  },
+  {
+    "question": "1 X 5", 
+    "answer": "5"
+  },
+  {
+    "question": "1 X 6", 
+    "answer": "6"
+  },
+  {
+    "question": "1 X 7", 
+    "answer": "7"
+  },
+  {
+    "question": "1 X 8", 
+    "answer": "8"
+  },
+  {
+    "question": "1 X 9", 
+    "answer": "9"
+  },
+  {
+    "question": "1 X 10", 
+    "answer": "10"
+  },
+  {
+    "question": "1 X 11", 
+    "answer": "11"
+  },
+  {
+    "question": "1 X 12", 
+    "answer": "12"
+  },
+  {
+    "question": "2 X 1", 
+    "answer": "2"
+  },
+  {
+    "question": "2 X 2", 
+    "answer": "4"
+  },
+  {
+    "question": "2 X 3", 
+    "answer": "6"
+  },
+  {
+    "question": "2 X 4", 
+    "answer": "8"
+  },
+  {
+    "question": "2 X 5", 
+    "answer": "10"
+  },
+  {
+    "question": "2 X 6", 
+    "answer": "12"
+  },
+  {
+    "question": "2 X 7", 
+    "answer": "14"
+  },
+  {
+    "question": "2 X 8", 
+    "answer": "16"
+  },
+  {
+    "question": "2 X 9", 
+    "answer": "18"
+  },
+  {
+    "question": "2 X 10", 
+    "answer": "20"
+  },
+  {
+    "question": "2 X 11", 
+    "answer": "22"
+  },
+  {
+    "question": "2 X 12", 
+    "answer": "24"
+  },
+  {
+    "question": "3 X 1", 
+    "answer": "3"
+  },
+  {
+    "question": "3 X 2", 
+    "answer": "6"
+  },
+  {
+    "question": "3 X 3", 
+    "answer": "9"
+  },
+  {
+    "question": "3 X 4", 
+    "answer": "12"
+  },
+  {
+    "question": "3 X 5", 
+    "answer": "15"
+  },
+  {
+    "question": "3 X 6", 
+    "answer": "18"
+  },
+  {
+    "question": "3 X 7", 
+    "answer": "21"
+  },
+  {
+    "question": "3 X 8", 
+    "answer": "24"
+  },
+  {
+    "question": "3 X 9", 
+    "answer": "27"
+  },
+  {
+    "question": "3 X 10", 
+    "answer": "30"
+  },
+  {
+    "question": "3 X 11", 
+    "answer": "33"
+  },
+  {
+    "question": "3 X 12", 
+    "answer": "36"
+  },
+  {
+    "question": "4 X 1", 
+    "answer": "4"
+  },
+  {
+    "question": "4 X 2", 
+    "answer": "8"
+  },
+  {
+    "question": "4 X 3", 
+    "answer": "12"
+  },
+  {
+    "question": "4 X 4", 
+    "answer": "16"
+  },
+  {
+    "question": "4 X 5", 
+    "answer": "20"
+  },
+  {
+    "question": "4 X 6", 
+    "answer": "24"
+  },
+  {
+    "question": "4 X 7", 
+    "answer": "28"
+  },
+  {
+    "question": "4 X 8", 
+    "answer": "32"
+  },
+  {
+    "question": "4 X 9", 
+    "answer": "36"
+  },
+  {
+    "question": "4 X 10", 
+    "answer": "40"
+  },
+  {
+    "question": "4 X 11", 
+    "answer": "44"
+  },
+  {
+    "question": "4 X 12", 
+    "answer": "48"
+  },
+  {
+    "question": "5 X 1", 
+    "answer": "5"
+  },
+  {
+    "question": "5 X 2", 
+    "answer": "10"
+  },
+  {
+    "question": "5 X 3", 
+    "answer": "15"
+  },
+  {
+    "question": "5 X 4", 
+    "answer": "20"
+  },
+  {
+    "question": "5 X 5", 
+    "answer": "25"
+  },
+  {
+    "question": "5 X 6", 
+    "answer": "30"
+  },
+  {
+    "question": "5 X 7", 
+    "answer": "35"
+  },
+  {
+    "question": "5 X 8", 
+    "answer": "40"
+  },
+  {
+    "question": "5 X 9", 
+    "answer": "45"
+  },
+  {
+    "question": "5 X 10", 
+    "answer": "50"
+  },
+  {
+    "question": "5 X 11", 
+    "answer": "55"
+  },
+  {
+    "question": "5 X 12", 
+    "answer": "60"
+  },
+  {
+    "question": "6 X 1", 
+    "answer": "6"
+  },
+  {
+    "question": "6 X 2", 
+    "answer": "12"
+  },
+  {
+    "question": "6 X 3", 
+    "answer": "18"
+  },
+  {
+    "question": "6 X 4", 
+    "answer": "24"
+  },
+  {
+    "question": "6 X 5", 
+    "answer": "30"
+  },
+  {
+    "question": "6 X 6", 
+    "answer": "36"
+  },
+  {
+    "question": "6 X 7", 
+    "answer": "42"
+  },
+  {
+    "question": "6 X 8", 
+    "answer": "48"
+  },
+  {
+    "question": "6 X 9", 
+    "answer": "54"
+  },
+  {
+    "question": "6 X 10", 
+    "answer": "60"
+  },
+  {
+    "question": "6 X 11", 
+    "answer": "66"
+  },
+  {
+    "question": "6 X 12", 
+    "answer": "72"
+  },
+]
+
+
+
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const card = document.querySelector(".card__inner");
 
+
+
 card.addEventListener("click", function (e) {
   card.classList.toggle('is-flipped');
 });
