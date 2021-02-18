b35b4f08836eab667463b98ad647a56a461a6339
alyssahellrung <alyssahellrung@gmail.com>
2021-02-18 09:55:41 -0800

    added Durstenfeld shuffle to shuffle question array

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const card = document.querySelector(".card__inner");
 const question = document.getElementById("#question")
 const answer = document.getElementById("#answer");
 
-
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+  console.log(array[1]);
+}
+shuffleArray(questions);
 
 card.addEventListener("click", function (e) {
   card.classList.toggle('is-flipped');
