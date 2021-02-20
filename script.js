701685456c7c2805e6519926c2c9f8e4f3c266aa
alyssahellrung <alyssahellrung@gmail.com>
2021-02-20 12:59:03 -0800

    array shuffles, can access first and second question

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const card = document.querySelector(".card__inner");
-const question = document.getElementById("#question")
-const answer = document.getElementById("#answer");
-// const button = document.getElementById("#nextCard");
+const question = document.getElementById("question")
+const answer = document.getElementById("answer");
+const button = document.getElementById("nextCard");
 
 function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
+  for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
       array[i] = array[j];
       array[j] = temp;
   }
-  console.log(array[1]);
+  return array;
 }
+
 shuffleArray(questions);
 
-// button.addEventListener("click", function(e) {
-//   console.log("button clicked");
-//   nextCard();
-// });
+function getQuestion() {
+  question.innerHTML = `<h2>${questions[0]["question"]}</h2>`;
+  answer.innerHTML = `<h3>${questions[0]["answer"]}</h3>;`
+}
+getQuestion();
+
+function nextCard() {
+  for (let i = 1; i < questions.length; i++) {
+  question.innerHTML = `<h2>${questions[i]["question"]}</h2>`;
+  answer.innerHTML = `<h3>${questions[i]["answer"]}</h3>`;
+  }
+}
+
+button.addEventListener("click", function(e) {
+  nextCard();
+});
 
 card.addEventListener("click", function (e) {
   card.classList.toggle('is-flipped');
