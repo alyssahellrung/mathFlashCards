e059912c9a9aeebf686262be1407d496b6489951
alyssahellrung <alyssahellrung@gmail.com>
2021-02-28 17:54:17 -0800

    created data2 file, started adding geography data

---
+++ data2.js
@@ -0,0 +1,114 @@
+const questions = [
+  {
+    "question": "Afghanistan", 
+    "answer": "Kabul"
+  },
+  {
+    "question": "Albania", 
+    "answer": "Tirana"
+  },
+  {
+    "question": "Algeria", 
+    "answer": "Algiers"
+  },
+  {
+    "question": "Andorra", 
+    "answer": "Andorra la Vella"
+  },
+  {
+    "question": "Angola", 
+    "answer": "Luanda"
+  },
+  {
+    "question": "Antigua and Barbuda", 
+    "answer": "St. John's"
+  },
+  {
+    "question": "Argentina", 
+    "answer": "Buenos Aires"
+  },
+  {
+    "question": "Armenia", 
+    "answer": "Yerevan"
+  },
+  {
+    "question": "Australia", 
+    "answer": "Canberra"
+  },
+  {
+    "question": "Austria", 
+    "answer": "Vienna"
+  },
+  {
+    "question": "Azerbaijan", 
+    "answer": "Baku"
+  },
+  {
+    "question": "The Bahamas", 
+    "answer": "Nassau"
+  },
+  {
+    "question": "Bahrain", 
+    "answer": "Manama"
+  },
+  {
+    "question": "Bangladesh", 
+    "answer": "Dhaka"
+  },
+  {
+    "question": "Barbados", 
+    "answer": "Bridgetown"
+  },
+  {
+    "question": "Belarus", 
+    "answer": "Minsk"
+  },
+  {
+    "question": "Belgium", 
+    "answer": "Brussels"
+  },
+  {
+    "question": "Belize", 
+    "answer": "Belmopan"
+  },
+  {
+    "question": "Benin", 
+    "answer": "Porto-Novo"
+  },
+  {
+    "question": "Bhutan", 
+    "answer": "Thimphu"
+  },
+  {
+    "question": "Bolivia", 
+    "answer": "Sucre"
+  },
+  {
+    "question": "Bosnia and Herzegovina", 
+    "answer": "Sarajevo"
+  },
+  {
+    "question": "Botswana", 
+    "answer": "Gaborone"
+  },
+  {
+    "question": "Brazil", 
+    "answer": "Brasilia"
+  },
+  {
+    "question": "Brunei", 
+    "answer": "Bandar Seri Begawan"
+  },
+  {
+    "question": "Bulgaria", 
+    "answer": "Sofia"
+  },
+  {
+    "question": "Burkina Faso", 
+    "answer": "Ouagadougou"
+  },
+  {
+    "question": "Burundi", 
+    "answer": "Bujumbura"
+  }
+]
